50b6b37422bd79a3f0bceb1ea8a4c9bf7787beb8
Saad <saadahmedscse@gmail.com>
2023-01-18 00:42:16 +0600

    Added getProducts function

--- app/src/main/java/com/saadahmedsoft/fakestoreapp/api/ApiService.kt
+++ app/src/main/java/com/saadahmedsoft/fakestoreapp/api/ApiService.kt
@@ -1,5 +1,12 @@
 package com.saadahmedsoft.fakestoreapp.api
 
+import com.saadahmedsoft.fakestoreapp.services.model.product.ProductResponse
+import io.reactivex.Observable
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface ApiService {
 
+    @GET("products")
+    fun getProducts(@Query("limit") limit: String): Observable<List<ProductResponse>>
 }
