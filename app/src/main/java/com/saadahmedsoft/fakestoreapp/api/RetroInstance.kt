2aa80abec049abe90a2b787fc5a9f9d76445aee8
Saad <saadahmedscse@gmail.com>
2023-01-18 00:45:04 +0600

    Added callAdapterFactory for using RxJava

--- app/src/main/java/com/saadahmedsoft/fakestoreapp/api/RetroInstance.kt
+++ app/src/main/java/com/saadahmedsoft/fakestoreapp/api/RetroInstance.kt
@@ -3,6 +3,7 @@ package com.saadahmedsoft.fakestoreapp.api
 import com.google.gson.GsonBuilder
 import com.saadahmedsoft.base.utils.Constants.Api.BASE_URL
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetroInstance {
@@ -11,6 +12,7 @@ object RetroInstance {
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create(gson))
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
         .build()
 
     val instance: ApiService by lazy {
