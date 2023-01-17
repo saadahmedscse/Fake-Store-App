db078e6421dce87c97b1df03f9e49720e214ff18
Saad <saadahmedscse@gmail.com>
2023-01-18 00:44:49 +0600

    Observe product list

--- app/src/main/java/com/saadahmedsoft/fakestoreapp/view/dashboard/tabs/products/ProductsFragment.kt
+++ app/src/main/java/com/saadahmedsoft/fakestoreapp/view/dashboard/tabs/products/ProductsFragment.kt
@@ -1,20 +1,38 @@
 package com.saadahmedsoft.fakestoreapp.view.dashboard.tabs.products
 
 import android.os.Bundle
+import android.util.Log
 import com.saadahmedsoft.base.BaseFragment
 import com.saadahmedsoft.fakestoreapp.databinding.FragmentProductsBinding
+import com.saadahmedsoft.fakestoreapp.utils.ProgressDialog
+import com.saadahmedsoft.kotlinhelper.helper.observe
+import com.saadahmedsoft.kotlinhelper.utils.DataState
 
 class ProductsFragment : BaseFragment<FragmentProductsBinding>(FragmentProductsBinding::inflate) {
     override val title: String
-        get() = "Home"
+        get() = "Fake Store Products"
     override val isBackButtonVisible: Boolean
         get() = false
 
     override fun onFragmentCreate(savedInstanceState: Bundle?) {
-        //
+        appViewModel.getProducts("5")
     }
 
     override fun observeData() {
-        //
+        observe(appViewModel.products) {
+            when (it) {
+                is DataState.Loading -> {
+                    ProgressDialog.show(requireContext())
+                }
+                is DataState.Success -> {
+                    Log.d("observable_debug", it.data?.get(0)?.title!!)
+                    ProgressDialog.dismiss()
+                }
+                is DataState.Failed -> {
+                    it.message?.let { err -> longSnackBar(err) }
+                    ProgressDialog.dismiss()
+                }
+            }
+        }
     }
 }
