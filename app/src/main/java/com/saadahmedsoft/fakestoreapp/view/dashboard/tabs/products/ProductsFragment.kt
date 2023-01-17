934c515eb175d4d7e8cdcff97eebb344a9c28cf2
Saad <saadahmedscse@gmail.com>
2023-01-18 02:31:11 +0600

    Implemented pagination and product search

--- app/src/main/java/com/saadahmedsoft/fakestoreapp/view/dashboard/tabs/products/ProductsFragment.kt
+++ app/src/main/java/com/saadahmedsoft/fakestoreapp/view/dashboard/tabs/products/ProductsFragment.kt
@@ -1,7 +1,11 @@
 package com.saadahmedsoft.fakestoreapp.view.dashboard.tabs.products
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.OnScrollListener
 import com.saadahmedsoft.base.BaseFragment
 import com.saadahmedsoft.fakestoreapp.databinding.FragmentProductsBinding
 import com.saadahmedsoft.fakestoreapp.services.model.product.ProductResponse
@@ -24,14 +28,29 @@ class ProductsFragment : BaseFragment<FragmentProductsBinding>(FragmentProductsB
     }
 
     private var productList: List<ProductResponse> = arrayListOf()
+    private var limit = 10
+    private var previousListSize = 0 //Added this for checking is more data available after pagination or not
+    private var moreDataAvailable = true
 
     override fun onFragmentCreate(savedInstanceState: Bundle?) {
         binding.recyclerView.setStaggeredGridLayoutManager(2)
         binding.recyclerView.adapter = adapter
 
         if (requireContext().internetAvailable()) {
-            appViewModel.getProducts("20")
+            appViewModel.getProducts(limit.toString())
         }
+
+        paginate()
+
+        binding.etSearchBox.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                filterItem(s.toString().lowercase())
+            }
+        })
     }
 
     override fun observeData() {
@@ -43,14 +62,44 @@ class ProductsFragment : BaseFragment<FragmentProductsBinding>(FragmentProductsB
                 is DataState.Success -> {
                     ProgressDialog.dismiss()
                     it.data?.let { items -> productList = items }
+
+                    if (previousListSize == productList.size) {
+                        shortSnackBar("No more item found")
+                        moreDataAvailable = false
+                    }
+                    else previousListSize = productList.size
+
                     adapter.addItems(productList)
                 }
                 is DataState.Failed -> {
-                    it.message?.let { err -> longSnackBar(err) }
+                    it.message?.let { err -> longSnackBar(if (err == "timeout") "Request Timeout" else err) }
                     ProgressDialog.dismiss()
                 }
             }
         }
+    }
+
+    private fun paginate() {
+        binding.recyclerView.addOnScrollListener(object : OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+                if (!recyclerView.canScrollVertically(1) && moreDataAvailable) {
+                    limit *= 2
+                    appViewModel.getProducts(limit.toString())
+                }
+            }
+        })
+    }
+
+    private fun filterItem(txt: String) {
+        val fList = arrayListOf<ProductResponse>()
+
+        for (item in productList) {
+            if (item.title?.lowercase()?.contains(txt)!!) fList.add(item)
+        }
+
+        adapter.addItems(fList)
     }
 
     override fun onItemClickListener(view: View, item: ProductResponse, position: Int) {
--- app/src/main/java/com/saadahmedsoft/fakestoreapp/view/dashboard/tabs/products/adapter/ProductAdapter.kt
+++ app/src/main/java/com/saadahmedsoft/fakestoreapp/view/dashboard/tabs/products/adapter/ProductAdapter.kt
@@ -5,6 +5,10 @@ import com.saadahmedsoft.fakestoreapp.R
 import com.saadahmedsoft.fakestoreapp.databinding.ItemLayoutProductBinding
 import com.saadahmedsoft.fakestoreapp.services.model.product.ProductResponse
 import com.saadahmedsoft.interfaces.OnItemActionListener
+import com.saadahmedsoft.kotlinhelper.helper.invisible
+import com.saadahmedsoft.kotlinhelper.helper.onClicked
+import com.saadahmedsoft.kotlinhelper.helper.onLongPressed
+import com.saadahmedsoft.kotlinhelper.helper.visible
 import com.squareup.picasso.Picasso
 
 class ProductAdapter(private val listener: OnItemActionListener<ProductResponse>) : BaseRecyclerAdapter<ProductResponse, ItemLayoutProductBinding>() {
@@ -14,5 +18,17 @@ class ProductAdapter(private val listener: OnItemActionListener<ProductResponse>
     override fun onBind(binding: ItemLayoutProductBinding, item: ProductResponse, position: Int) {
         binding.item = item
         Picasso.get().load(item.image).into(binding.ivProductImage)
+
+        if (position % 3 == 0) binding.tvOutOfStock.visible()
+        else binding.tvOutOfStock.invisible()
+
+        binding.root.onClicked {
+            listener.onItemClickListener(it, item, position)
+        }
+
+        binding.root.onLongPressed {
+            listener.onItemLongPressListener(it, item, position)
+            true
+        }
     }
 }
