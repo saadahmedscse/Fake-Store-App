1855d47a953c95efe6078f845cf6fdb103b0d47c
Saad <saadahmedscse@gmail.com>
2023-01-18 00:42:56 +0600

    Get products in ViewModel

--- app/src/main/java/com/saadahmedsoft/fakestoreapp/viewmodel/AppViewModel.kt
+++ app/src/main/java/com/saadahmedsoft/fakestoreapp/viewmodel/AppViewModel.kt
@@ -1,7 +1,20 @@
-package com.saadahmedsoft.batingapp.viewmodel
+package com.saadahmedsoft.fakestoreapp.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.saadahmedsoft.fakestoreapp.services.model.product.ProductResponse
+import com.saadahmedsoft.fakestoreapp.services.repository.AppRepo
+import com.saadahmedsoft.kotlinhelper.utils.DataState
 
 class AppViewModel : ViewModel() {
-   //
+    private val repo = AppRepo()
+
+    private val _products = MutableLiveData<DataState<List<ProductResponse>>>()
+    val products: LiveData<DataState<List<ProductResponse>>>
+        get() = _products
+
+    fun getProducts(limit: String) {
+        repo.getProducts(limit, _products)
+    }
 }
