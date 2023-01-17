d91eec48a9ca748b7e86c03c7d6f07c52ead776f
Saad <saadahmedscse@gmail.com>
2023-01-18 03:46:41 +0600

    Set left information to product

--- app/src/main/java/com/saadahmedsoft/fakestoreapp/view/dashboard/tabs/products/adapter/ProductAdapter.kt
+++ app/src/main/java/com/saadahmedsoft/fakestoreapp/view/dashboard/tabs/products/adapter/ProductAdapter.kt
@@ -1,5 +1,7 @@
 package com.saadahmedsoft.fakestoreapp.view.dashboard.tabs.products.adapter
 
+import android.annotation.SuppressLint
+import android.graphics.Paint
 import com.saadahmedsoft.base.BaseRecyclerAdapter
 import com.saadahmedsoft.fakestoreapp.R
 import com.saadahmedsoft.fakestoreapp.databinding.ItemLayoutProductBinding
@@ -10,17 +12,25 @@ import com.saadahmedsoft.kotlinhelper.helper.onClicked
 import com.saadahmedsoft.kotlinhelper.helper.onLongPressed
 import com.saadahmedsoft.kotlinhelper.helper.visible
 import com.squareup.picasso.Picasso
+import java.text.DecimalFormat
 
 class ProductAdapter(private val listener: OnItemActionListener<ProductResponse>) : BaseRecyclerAdapter<ProductResponse, ItemLayoutProductBinding>() {
     override val layoutRes: Int
         get() = R.layout.item_layout_product
 
+    @SuppressLint("SetTextI18n")
     override fun onBind(binding: ItemLayoutProductBinding, item: ProductResponse, position: Int) {
         binding.item = item
         Picasso.get().load(item.image).into(binding.ivProductImage)
 
         if (position % 3 == 0) binding.tvOutOfStock.visible()
         else binding.tvOutOfStock.invisible()
+
+        binding.category.text = item.category?.let { capitalizeAllFirstChars(it) }
+
+        val tenPercentAdditionOfPrice = item.price!! * 10 / 100 + item.price!! //Though our response does not contains any other price, So I'm generating main product price by increasing the price by 10%
+        binding.tvMainPrice.text = "৳ ${DecimalFormat("##.##").format(tenPercentAdditionOfPrice)}"
+        binding.tvMainPrice.paintFlags = binding.tvMainPrice.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
 
         binding.root.onClicked {
             listener.onItemClickListener(it, item, position)
@@ -30,5 +40,17 @@ class ProductAdapter(private val listener: OnItemActionListener<ProductResponse>
             listener.onItemLongPressListener(it, item, position)
             true
         }
+    }
+
+    private fun capitalizeAllFirstChars(s: String): String {
+        val chars = s.toCharArray()
+        val sb = StringBuilder()
+
+        for (i in chars.indices) {
+            if (i == 0 || chars[i - 1] == ' ') sb.append(chars[i].uppercaseChar())
+            else sb.append(chars[i])
+        }
+
+        return sb.toString()
     }
 }
